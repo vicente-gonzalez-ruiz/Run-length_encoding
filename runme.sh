bebc817070ed3e99e2cf4070d31436d5711fc03b
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-12-31 17:22:17 +0100

    .

--- runme.sh
+++ runme.sh
@@ -1,10 +1,12 @@
 export BIBINPUTS=":.:/home/vruiz/bibs/:"
 make clean
 make index.pdf
-grep citation ~/Burrows-Wheeler_transform/index.aux >> index.aux
+rm -f citations
+grep citation ~/Burrows-Wheeler_transform/index.aux >> citations
 #echo "\bibdata{text-compression,data-compression}" >> index.aux
 #echo "\bibstyle{plain}" >> index.aux
 #pdflatex citations
+cat citations >> index.aux
 bibtex index
 #touch texput.tex
 make index.html
